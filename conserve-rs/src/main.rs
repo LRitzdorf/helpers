3b0bcf2fd40db8407080ce176162f2a9265ec4b5
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2025-04-20 11:02:02 -0700

    conserve: address style comments from Willow

--- conserve-rs/src/main.rs
+++ conserve-rs/src/main.rs
@@ -1,26 +1,29 @@
 use clap::{Command, command};
 use std::path::Path;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{self, prelude::*};
 
 
 // NOTE: Set your "battery conservation mode" file here:
 const TARGET: &str = "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode";
 
 
-fn get_mode(f: &mut File) -> std::io::Result<bool> {
+const MODE_OFF: &[u8; 2] = b"0\n";
+const MODE_ON:  &[u8; 2] = b"1\n";
+
+fn get_mode(f: &mut File) -> io::Result<bool> {
     let mut buf = [0u8; 2];
     f.read_exact(&mut buf)?;
     match &buf {
-        b"0\n" => Ok(false),
-        b"1\n" => Ok(true),
-        _      => panic!("Unexpected conservation mode '{}'", String::from_utf8_lossy(&buf)),
+        MODE_OFF => Ok(false),
+        MODE_ON  => Ok(true),
+        _        => panic!("Unexpected conservation mode '{}'", String::from_utf8_lossy(&buf)),
     }
 }
 
-fn set_mode(f: &mut File, state: bool) -> std::io::Result<bool> {
-    let buf = [b'0' + u8::from(state), b'\n'];
-    f.write_all(&buf)?;
+fn set_mode(f: &mut File, state: bool) -> io::Result<bool> {
+    let buf = if state { MODE_ON } else { MODE_OFF };
+    f.write_all(buf)?;
     Ok(state)
 }
 
@@ -62,11 +65,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Ok(file) => file,
     };
 
-    match m.subcommand() {
-        Some(("enable",  _))  => { println!("{}", set_mode(&mut f, true)?); }
-        Some(("disable", _))  => { println!("{}", set_mode(&mut f, false)?); }
-        Some(("toggle",  _))  => { let mode = get_mode(&mut f)?; println!("{}", set_mode(&mut f, !mode)?); }
-        _                     => { println!("{}", get_mode(&mut f)?); }
-    }
+    let result = match m.subcommand() {
+        Some(("enable",  _))  => { set_mode(&mut f, true)? }
+        Some(("disable", _))  => { set_mode(&mut f, false)? }
+        Some(("toggle",  _))  => { let mode = get_mode(&mut f)?; set_mode(&mut f, !mode)? }
+        _                     => { get_mode(&mut f)? }
+    };
+    println!("{}", result);
     Ok(())
 }
